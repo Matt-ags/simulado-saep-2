9d82080d146a026c00698b914dcf48537ef7f84a
GBTorto <moraisgabriel867@gmail.com>
2025-11-03 16:09:25 -0300

    criação dos models e db.

--- estoque/models.py
+++ estoque/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from usuario.models import Usuario
 # Create your models here.
+class Estoque(models.Model):
+    nome_produto = models.CharField(max_length=50)
+    qtd_minima = models.IntegerField()
+    qtd_produto = models.IntegerField()
+    tipo_produto = models.CharField(max_length=20)
+    tensao = models.CharField(max_length=10)
+    dimensao = models.CharField(max_length=15)
+    resolucao_tela = models.CharField(max_length=15)
+    armazenamento_gb = models.IntegerField()
+    conectividade = models.CharField(max_length=20)
+
+class Historico(models.Model):
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    id_estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE)
+    ESCOLHA_ACAO = [
+        ('DELETAR', 'Produto deletado'),
+        ('AUMENTAR', 'Produto adicionado ao estoque'),
+        ('RETIRADO', 'Produto retirado do estoque'),
+        ('ADICIONAR', 'Novo produto'),
+    ]
+    acao = models.CharField(max_length=20, choices=ESCOLHA_ACAO)
+    data_hora = models.DateTimeField(auto_now_add=True)
